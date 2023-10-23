a16b427c3b65e86c5d88db1802fcf2bcb5d0cdb1
nirkaufman <nirkaufman@gmail.com>
2023-10-23 10:47:31 +0300

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import * as http2 from "http2";
 
 export default function Home() {
   return (
+      <>
     <h1>IJS Munich - NextJS workshop</h1>
+     <h2>https://github.com/nirkaufman/ijs-munic</h2>
+     </>
   )
 }
+
