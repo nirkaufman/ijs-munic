69762a1f20e3094ad8571b2ffd7ed6ba0af865de
nirkaufman <nirkaufman@gmail.com>
2023-10-23 12:35:24 +0300

    dynamic routes

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 export default function BlogPage() {
   return (
     <div>
       <h1>Blog</h1>
       <p>This is the blog page.</p>
       <ul>
-        <li>post #1</li>
-        <li>post #2</li>
-        <li>post #3</li>
+        <Link href={`/blog/${1}`}><li>post #1</li></Link>
+        <Link href={`/blog/${2}`}><li>post #2</li></Link>
+        <Link href={`/blog/${3}`}><li>post #3</li></Link>
       </ul>
     </div>
   )
