2bab9f8af8f90ba09774914504accd3e9550ead2
nirkaufman <nirkaufman@gmail.com>
2023-10-23 11:23:33 +0300

    links

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
       <>
        <h1>IJS Munich - NextJS workshop</h1>
        <h2>https://github.com/nirkaufman/ijs-munic</h2>
-        <a href="/posts">go to blog</a>
-       <button>Click me</button>
+       <Link href="/blog">go to blog</Link>
      </>
   )
 }
